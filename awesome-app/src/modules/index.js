b7d613f2b9d4caf31059d692358cda6b4d9924a6
tefferson <teffersonguterres@gmail.com>
2019-05-10 18:44:30 -0300

    ajsutar export dos modules

--- awesome-app/src/modules/index.js
+++ awesome-app/src/modules/index.js
@@ -7,4 +7,11 @@ const SpeechPage = React.lazy(() => import('./speech/speech.page'))
 const CounterPage = React.lazy(() => import('./counter/counter.page'))
 const TicTacToePage = React.lazy(() => import('./tic-tac-toe/tic-tac-toe.page'))
 
-export { HomePage, CounterPage, TicTacToePage, PhotosPage, SpeechPage }
+export {
+	HomePage,
+	LoginPage,
+	PhotosPage,
+	SpeechPage,
+	CounterPage,
+	TicTacToePage
+}
